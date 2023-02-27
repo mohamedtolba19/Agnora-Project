22d509a5a15d6757493fba4edaacc91c89ee7f03
mohamedtolba19 <mohamedtolba9911@gmail.com>
2023-02-27 18:34:30 +0200

    seconed commit

--- js/main.js
+++ js/main.js
@@ -10,15 +10,20 @@ $(document).ready(function(){
           responsiveClass:true,
           responsive:{
               0:{
-                  items:2,
+                  items:1,
                   nav:true
               },
-              600:{
+              500:{
                   items:3,
                   nav:false
               },
+              700:{
+                  items:2,
+                  nav:true,
+                  loop:false
+              },
               1000:{
-                  items:5,
+                  items:3,
                   nav:true,
                   loop:false
               }
